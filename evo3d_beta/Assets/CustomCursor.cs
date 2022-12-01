47031c8d9fa8008c8bde616e8a37b34c57db9cef
OGBiggieCheese <manuel.casimiro.et26@gmail.com>
2022-11-30 22:06:14 -0300

    Modificacion

--- evo3d_beta/Assets/CustomCursor.cs
+++ evo3d_beta/Assets/CustomCursor.cs
@@ -7,7 +7,7 @@ public class CustomCursor : MonoBehaviour
 
     void Update()
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = mousePosition; 
     }
 }
